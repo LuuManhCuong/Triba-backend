174d9deb1a8ada87809d0a14f302640d7ab79dce
LuuManhCuong <cuong1042k1@gmail.com>
2024-05-11 22:21:16 +0700

    config scurity done

--- src/main/java/com/backend/triba/config/SecurityConfiguration.java
+++ src/main/java/com/backend/triba/config/SecurityConfiguration.java
@@ -5,10 +5,15 @@ import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -27,48 +32,59 @@ import lombok.RequiredArgsConstructor;
 @ComponentScan(basePackages = "com.backend.triba.exception")
 public class SecurityConfiguration {
 
+    private final UserServiceImpl userService;
+    private static final String[] WHITE_LIST_URL = {"/**", "/api/v1/auth/**", "/api/v1/stayeasy/**", "/api/v1/user/**"};
+    private static final String[] ADMIN_LIST_URL = {"/api/v1/token/**", "/api/v1/stayeasy/admin/**"};
+    private final JwtAuthenticationFilter jwtAuthFilter;
 
-	
-	private final UserServiceImpl userService;
-	
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
+                req -> req.requestMatchers(WHITE_LIST_URL).permitAll()
+                    .requestMatchers("/api/v1/owner/**").hasAnyRole("HOST")
+                    .requestMatchers(ADMIN_LIST_URL).hasAnyRole("ADMIN")
+                    .anyRequest().authenticated())
+            .exceptionHandling(configurer -> configurer
+                .accessDeniedHandler(new AccessDeniedExceptionHandler())
+                .authenticationEntryPoint(new AuthenticationExceptionHandler()))
+            .authenticationProvider(authenticationProvider())
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
+        return http.build();
+    }
 
-	private static final String[] WHITE_LIST_URL = {"/**", "/api/v1/auth/**", "/api/v1/stayeasy/**", "/api/v1/user/**"};
-	private static final String[] ADMIN_LIST_URL = {"/api/v1/token/**", "/api/v1/stayeasy/admin/**"};
-	private final JwtAuthenticationFilter jwtAuthFilter;
-	private final AuthenticationProvider authenticationProvider;
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService.userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-		.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(
-						req -> req.requestMatchers(WHITE_LIST_URL).permitAll()
-						.requestMatchers("/api/v1/owner/**").hasAnyRole("HOST")
-						.requestMatchers(ADMIN_LIST_URL).hasAnyRole("ADMIN")
-						.anyRequest().authenticated())
-				.exceptionHandling(configurer -> configurer
-				        .accessDeniedHandler(new AccessDeniedExceptionHandler())
-				        .authenticationEntryPoint(new AuthenticationExceptionHandler())
-				        )
-				.authenticationProvider(authenticationProvider)
-				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-			
-		return http.build();
-	}
-	
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+    	return configuration.getAuthenticationManager();
+    }
 
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
-		corsConfiguration.setAllowCredentials(true);
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfiguration);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+        corsConfiguration.setAllowCredentials(true);
 
-		return source;
-	}
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
 }
